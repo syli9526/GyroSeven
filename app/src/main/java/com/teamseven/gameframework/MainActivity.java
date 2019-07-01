926b235bb9bd4392f314fb5032fa2f6a4687a191
Moraeso <45632773+Moraeso@users.noreply.github.com>
2019-07-01 10:35:09 +0900

    Set application fullscreen

--- app/src/main/java/com/teamseven/gameframework/MainActivity.java
+++ app/src/main/java/com/teamseven/gameframework/MainActivity.java
@@ -2,6 +2,9 @@ package com.teamseven.gameframework;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.teamseven.gyroseven.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(new GameView(this));
-
-
     }
 }
